aa8ffaff99ff197aa615383aff4e572e640783c6
umrgrs <qxrvynilp@gmail.com>
2024-10-31 11:55:11 -0600

    Buttons color changes

---
+++ src/app/machines-list/machines-list/machines-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-machines-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './machines-list.component.html',
+  styleUrl: './machines-list.component.css'
+})
+export class MachinesListComponent {
+
+}
